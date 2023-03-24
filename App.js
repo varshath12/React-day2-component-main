6576321cf7f4810a84ba5a040f733c4d35d715c4
varshath12 <108916881+varshath12@users.noreply.github.com>
2023-03-24 11:40:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import World from './Component/World';
+
+
+function App() {
+  return (
+    <div className="App">
+      This Our Project
+<World/>
+    </div>
+  );
+}
+
+export default App;
---
+++ World.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function World() {
+  return (
+    <div><p>This Is World Functional Component</p></div>
+  )
+}
+
+export default World
